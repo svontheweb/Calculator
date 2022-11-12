35a382b4dfdc1d895bd24b5fe7fa637a666223d2
V <svontheweb@gmail.com>
2022-11-12 17:25:44 +0530

    gave id to every button, fixed i/p o/p area display(i/p above, o/p below), set function to every key except =(to display particular key in i/p display a rea /

--- app/src/main/java/com/example/ktcalculator/MainActivity.kt
+++ app/src/main/java/com/example/ktcalculator/MainActivity.kt
@@ -2,10 +2,114 @@ package com.example.ktcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ktcalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //setting functions to key in calculator to manipulate display area
+        // such as, pressing a no shows it in display
+        // or, AC button clears display areas
+        binding.btnAc.setOnClickListener() {
+            binding.tvInputTextDisplay.text = ""
+            binding.tvOutputTextDisplay.text = ""
+        }
+
+        binding.btnStartBracket.setOnClickListener() {
+            binding.tvInputTextDisplay.append("(")
+        }
+
+        binding.btnEndBracket.setOnClickListener() {
+            binding.tvInputTextDisplay.append(")")
+        }
+
+        binding.btnDivide.setOnClickListener() {
+            binding.tvInputTextDisplay.append("/")
+        }
+
+
+
+        binding.btn7.setOnClickListener() {
+            binding.tvInputTextDisplay.append("7")
+        }
+
+        binding.btn8.setOnClickListener() {
+            binding.tvInputTextDisplay.append("8")
+        }
+
+        binding.btn9.setOnClickListener() {
+            binding.tvInputTextDisplay.append("9")
+        }
+
+        binding.btnMultiply.setOnClickListener() {
+            binding.tvInputTextDisplay.append("*")
+        }
+
+
+
+
+        binding.btn4.setOnClickListener() {
+            binding.tvInputTextDisplay.append("4")
+        }
+
+        binding.btn5.setOnClickListener() {
+            binding.tvInputTextDisplay.append("5")
+        }
+
+        binding.btn6.setOnClickListener() {
+            binding.tvInputTextDisplay.append("6")
+        }
+
+        binding.btnSubtract.setOnClickListener() {
+            binding.tvInputTextDisplay.append("-")
+        }
+
+
+
+
+        binding.btn1.setOnClickListener() {
+            binding.tvInputTextDisplay.append("1")
+        }
+
+        binding.btn2.setOnClickListener() {
+            binding.tvInputTextDisplay.append("2")
+        }
+
+        binding.btn3.setOnClickListener() {
+            binding.tvInputTextDisplay.append("3")
+        }
+
+        binding.btnAdd.setOnClickListener() {
+            binding.tvInputTextDisplay.append("+")
+        }
+
+
+
+
+
+
+        binding.btn0.setOnClickListener() {
+            binding.tvInputTextDisplay.append("0")
+        }
+
+        binding.btnDecimal.setOnClickListener() {
+            binding.tvInputTextDisplay.append(".")
+        }
+
+        binding.btnEqual.setOnClickListener() {
+            // do not show EQUAL SIGN insted calculate on click
+        // binding.tvInputTextDisplay.append("=")
+
+
+        }
+
+
     }
 }
